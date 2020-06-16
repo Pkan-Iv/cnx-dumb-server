fe212ab86bdefe88c35d8b96234c8c985e0195a4
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-06-16 17:13:47 +0200

    Made GET /reports uuid optionnal again

--- routes.js
+++ routes.js
@@ -3,8 +3,16 @@ import express from 'express'
 const router = express.Router()
 
 router.post( '/customer_reports', (req, res) => {
-  console.info('customers report received:\n%o', req.body)
-  res.status(200).json()
+  const { body } = req,
+        { apikey, ar_result, ar_status, ar_time, called, uuid } = body
+  
+  if(apikey & ar_result & ar_status & ar_time & called & uuid) {
+    console.info('customers report received:\n%o', req.body)
+    res.status(200).json()
+  } else {
+    console.error('One or more inputs missing.')
+    return res.status(404).json()
+  }
 } )
 
 export default router
