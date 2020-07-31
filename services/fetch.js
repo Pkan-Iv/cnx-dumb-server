8f0128cb3219a3f939049ebc14460ba4a816352b
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-07-31 10:46:25 +0200

    Upload file and csvtojson added

--- services/fetch.js
+++ services/fetch.js
@@ -1,5 +1,11 @@
 import Fetch from 'cross-fetch'
+import Path from 'path'
 import { logger } from '../index'
+
+import * as Config from '../config/config.json'
+import { csvToJSON, toNumber } from '../utils/toolkit'
+
+const { target } = Config
 
 export default {
   async fetch_create_index_data(req, res, next, {
@@ -46,6 +52,45 @@ export default {
     })
     next()
     return body.data, body.index
+  },
+
+  async fetch_file_to_upload (req, res, next, {
+    log = logger({ module: ' fetch ' }),
+  } = {}) {
+    const { files } = req,
+          datas = []
+
+    if (!files || Object.keys( files ).length === 0) {
+      log.warning({ status: false, reason: 'No files found to upload.' })
+      return res.status(204).json()
+    }
+
+    const data = Object.keys( files ).forEach( (file) => {
+      if ( files[file].length ) {
+        return files[file].map( (data) => {
+          const { mimetype, mv, name, size } = data
+          mv( Path.join( target, name ))
+          datas.push({ name, mimetype, size })
+
+          const names = datas.map( ({name}) => name).join(',')   
+          log.debug({ status: true, message: "Files '" + names +"' uploaded." })
+          return files[file]
+        })
+      }      
+
+      const { mimetype, mv, name, size } = files[ file ]
+      mv( Path.join( target, name ))
+      datas.push({ name, mimetype, size })
+
+      log.debug({ status: true, message: "File '" + name +"' uploaded." })
+
+      return files[file]
+    })  
+    
+    // res.json({ status: true, message: "File(s) '" + names +"' uploaded." })
+    next()
+
+    return data
   },
 
   async fetch_update_index_data(req, res, next, {
@@ -146,5 +191,33 @@ export default {
 
     next()
     return body.data, body.index
+  },
+  
+  async get_json_from_csv_files(req, res, next, {
+    log = logger({ module: ' fetch ' }),
+  } = {}) {
+    const { body, files } = req,
+          check = Buffer.isBuffer(files.files.data)
+
+    try {
+      if (check) {
+  
+        const csv = files.files.data.toString()
+        
+        body.data = csvToJSON(csv, ';')
+        body.index = files.files.name
+        
+        //res.json(csvToJSON(csv, ';'))
+        
+        
+        next()
+        log.debug('Change CSV data to JSON. Now transferring to ElasticSearch')
+        
+        return body
+      }
+    } catch (error) {
+      log.error(error)
+      return res.status(500).json({ reason: error})      
+    }
   }
 }
