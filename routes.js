989e3125b5ac4f4a3177ee350b47650981d4d3ea
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-08-21 11:31:28 +0200

    Updated /customer_reports route

--- routes.js
+++ routes.js
@@ -50,13 +50,14 @@ export function Webhook(logger) {
     const { body } = req,
     { apikey, ar_result, ar_status, ar_time, called, uuid } = body
     
-    if(apikey & ar_result & ar_status & ar_time & called & uuid) {
-      console.info('customers report received:\n%o', req.body)
-      res.status(200).json()
-    } else {
-      console.error('One or more inputs missing.')
-      return res.status(404).json()
-    }
+    if(apikey && ar_result && ar_status && ar_time && called && uuid) {
+      log.info('customers report received: %o', body)
+      log.info('message ', apikey, ar_result, body)
+      return res.status(200).json()
+    } 
+    log.error('One or more inputs missing.')
+    return res.status(404).json()
+    
   } )
   
   return router
